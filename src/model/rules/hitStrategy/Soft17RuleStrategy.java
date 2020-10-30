a31cc1624330bc5072dd9ccfcb22fa01a5dfb13b
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-30 15:15:15 +0100

    Create Soft17RuleStrategy.java

---
+++ src/model/rules/hitStrategy/Soft17RuleStrategy.java
@@ -0,0 +1,33 @@
+package model.rules.hitStrategy;
+
+import model.Card;
+import model.Player;
+import model.rules.IVisitor;
+import model.rules.hitStrategy.IHitStrategy;
+
+public class Soft17RuleStrategy implements IHitStrategy {
+
+    @Override
+    public boolean DoHit(Player a_dealer) {
+       if(isSoft17(a_dealer)) {
+           if(a_dealer.CalcScore() == 17) {
+               return true;
+           }
+       }
+           return a_dealer.CalcScore() <17;
+    }
+
+    private boolean isSoft17(Player a_dealer){
+
+            for(Card c : a_dealer.GetHand()){
+                if(c.GetValue() == Card.Value.Ace)
+                    return true;
+            }
+
+        return false;
+    }
+    @Override
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+}
