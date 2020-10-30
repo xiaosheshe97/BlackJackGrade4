c650a31bafafe09499f300fcc6d7d880f693a7a3
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-30 15:14:54 +0100

    Create DealerWinStrategy.java

---
+++ src/model/rules/winnerStrategy/DealerWinStrategy.java
@@ -0,0 +1,19 @@
+package model.rules.winnerStrategy;
+
+import model.rules.IVisitor;
+
+public class DealerWinStrategy implements IWinnerStrategy {
+    @Override
+    public boolean isDealerWinner(int dealerCore, int playerScore,int maxScore) {
+        if (playerScore > maxScore) {
+            return true;
+        } else if (dealerCore > maxScore) {
+            return false;
+        }
+        return dealerCore == playerScore;
+    }
+    @Override
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+}
