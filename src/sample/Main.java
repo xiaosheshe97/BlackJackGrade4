8e105f14c047d70a0a75a771ce6fb76464806ad8
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-31 00:52:58 +0100

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/PlayNewGame.fxml"));
         primaryStage.setTitle("BlackJack");
-        primaryStage.setScene(new Scene(root, 600, 275));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
