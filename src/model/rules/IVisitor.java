bdf6337a643542f12919ebee6a89ebf1fb1568f3
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-30 15:15:05 +0100

    Create IVisitor.java

---
+++ src/model/rules/IVisitor.java
@@ -0,0 +1,19 @@
+package model.rules;
+
+import model.rules.hitStrategy.BasicHitStrategy;
+import model.rules.hitStrategy.Soft17RuleStrategy;
+import model.rules.newGameStartegy.AmericanNewGameStrategy;
+import model.rules.newGameStartegy.InternationalNewGameStrategy;
+import model.rules.winnerStrategy.DealerWinStrategy;
+import model.rules.winnerStrategy.PlayerWinStrategy;
+
+public interface IVisitor {
+    void visit(AmericanNewGameStrategy americanNewGameStrategy);
+    void visit(InternationalNewGameStrategy internationalNewGameStrategy);
+    void visit(BasicHitStrategy basicHitStrategy);
+    void visit(Soft17RuleStrategy Soft17RuleStrategy);
+    void visit(DealerWinStrategy DealerWinStrategy);
+    void visit(PlayerWinStrategy PlayerWinStrategy);
+
+}
+
