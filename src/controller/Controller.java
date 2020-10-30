4aab54135cc80860d95b59818470be064a158ab6
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-30 15:14:42 +0100

    Update Controller.java

---
+++ src/controller/Controller.java
@@ -0,0 +1,30 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    private Button playGameButton;
+
+    @FXML
+    void PlayGame(ActionEvent event) throws IOException {
+        Parent playGameView = FXMLLoader.load(getClass().getResource("../view/PlayNewGame.fxml"));
+        
+        Scene playScene = new Scene(playGameView);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(playScene);
+        window.show();
+    }
+
+}
