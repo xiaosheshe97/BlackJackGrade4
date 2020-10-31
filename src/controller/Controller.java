952a7e452ae16e4106ce5487b5ed5cbc16e0d91a
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-31 16:54:08 +0100

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,32 +1,47 @@
 package controller;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import model.Card;
+import model.Dealer;
 import model.Game;
 import model.ICardObserver;
-import view.IView;
+import model.rules.BasicHitAmericanGameDealerWinFactory;
+import model.rules.IVisitor;
+import model.rules.RulesAbstractFactory;
+import view.PrintVisitor;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller{
+public class Controller implements Initializable, ICardObserver{
+    private RulesAbstractFactory factory;
+    private IVisitor iVisitor;
     private Game a_game;
 
     private Image[] deckBack = {new Image("model/PNG/blue_back.png"), new Image("model/PNG/gray_back.png"), new Image("model/PNG/green_back.png"),
             new Image("model/PNG/purple_back.png"), new Image("model/PNG/red_back.png"), new Image("model/PNG/yellow_back.png")};
+
+    private String cardType;
+
+    private Image card;
+
+    @Override
+    public void updateNewCard(Card card) {
+        String cardScores[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        assert (cardScores.length == Card.Value.Count.ordinal()) : "Card Scores array size does not match number of card values";
+        card.Show(true);
+        this.cardType = cardScores[card.GetValue().ordinal()] + card.GetColor().toString().charAt(0);
+        System.out.println(this.cardType);
+        this.card = new Image("model/PNG/" + this.cardType + ".png");
+        Pause();
+    }
 
     @FXML
     private Button PlayNewGame;
@@ -83,40 +98,50 @@ public class Controller{
     private ImageView PlayerCard6;
 
 
-//    @Override
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        this.Deck.setImage(deckBack[0]);
-//        this.DealerCard1.setImage(null);
-//    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.factory = new BasicHitAmericanGameDealerWinFactory();
+        this.iVisitor = new PrintVisitor();
+        this.a_game = new Game(factory, iVisitor);
+        this.a_game.SubscriptionToNewCards(this);
+        DeckBack();
+    }
 
     @FXML
-    void PlayNewGame(ActionEvent event) throws IOException {
-//        this.a_game.NewGame();
-//        this.Deck.setImage(deckBack[0]);
-//
-//        Card card = new Card(Card.Color.Clubs, Card.Value.Two);
-//        card.Show(true);
-//        String cardType = card.GetValue() + "of" + card.GetColor();
-//        System.out.println(cardType);
-//        Card card2 = new Card(Card.Color.Diamonds, Card.Value.Two);
-//        card2.Show(true);
-//        String cardType2 = String.valueOf(card2.GetValue().ordinal()) + card2.GetColor().toString().charAt(0);
-////        DealerCard1.setImage(new Image("model/PNG/TwoofClubs.png"));
-//        System.out.println(cardType2);
-//
-//
-//
-//        DealerCard1.setImage(new Image("model/PNG/" + cardType + ".png"));
-//        DealerCard2.setImage(new Image("model/PNG/" + cardType2 + ".png"));
+    void PlayNewGame() {
+        this.a_game.NewGame();
+        PlayerCard1.setImage(this.card);
+        DeckBack();
+        DealerCard1.setImage(this.card);
+        DeckBack();
+        PlayerCard2.setImage(new Image("model/PNG/" + this.cardType + ".png"));
+        DeckBack();
+        DealerCard2.setImage(new Image("model/PNG/" + this.cardType + ".png"));
 
+        PlayerScore.setText(String.valueOf(a_game.GetPlayerScore()));
+        DealerScore.setText(String.valueOf(a_game.GetDealerScore()));
     }
 
     @FXML
     void Hit(ActionEvent event) throws IOException {
+        this.Deck.setImage(deckBack[(int)(Math.random() * 6)]);
+
     }
 
     @FXML
     void Stand(ActionEvent event) throws IOException {
+        this.Deck.setImage(deckBack[(int)(Math.random() * 6)]);
+
     }
 
+    public void Pause(){
+        try{
+            System.out.println("getting ....");
+            Thread.sleep(2000);
+        }catch (Exception e){}
+    }
+
+    private void DeckBack() {
+        this.Deck.setImage(deckBack[(int)(Math.random() * 6)]);
+    }
 }
